4c740d3314f490a710199c149cd8c0838f4540c4
Leonel Santos <leonelsantos.me@gmail.com>
2011-10-14 13:41:19 -0500

    invoice show template formatting

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,18 @@
 class Company < ActiveRecord::Base
   belongs_to :account
   validates :account_id, :presence => true
+  
+  def formatted
+    a = "#{name} <br />" unless name.blank?
+    a = "#{a} #{address_one} <br />" unless address_one.blank?
+    a = "#{a} #{address_two} <br />" unless address_two.blank?
+    a = "#{a} #{city} " unless self.city.blank?
+    a = "#{a} #{state} " unless state.blank?
+    a = "#{a} #{zipcode} <br />" unless zipcode.blank?
+    a = "#{a} #{phone} <br />" unless phone.blank?
+    a = "#{a} #{fax} <br />" unless fax.blank?
+    a = "#{a} #{website}" unless website.blank?
+    a
+  end
+
 end
